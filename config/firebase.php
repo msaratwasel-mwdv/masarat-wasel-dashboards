4023e0171512618e8948e8cb459ada7756dceac4
f2363692-commits (f2363692-commits) <f2363692@gmail.com>
2026-05-17 04:16:53 +0300

    Fix firebase config dynamic project key

--- config/firebase.php
+++ config/firebase.php
@@ -10,6 +10,12 @@ return [
     'default' => env('FIREBASE_PROJECT', 'app'),
 
     'projects' => [
+        env('FIREBASE_PROJECT', 'app') => [
+            'credentials' => env('FIREBASE_CREDENTIALS', storage_path('app/firebase/service-account.json')),
+        ],
+        'parent' => [ // Fallback in case env variable is explicitly "parent"
+            'credentials' => env('FIREBASE_CREDENTIALS', storage_path('app/firebase/service-account.json')),
+        ],
         'app' => [
             'credentials' => env('FIREBASE_CREDENTIALS', storage_path('app/firebase/service-account.json')),
         ],
