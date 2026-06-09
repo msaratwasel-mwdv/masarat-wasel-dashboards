45fa53293158dac4288494f5760afe6521ff0b37
f2363692-commits (f2363692-commits) <f2363692@gmail.com>
2026-06-10 01:18:53 +0300

    fix: ignore encryption for locale cookie so backend can read plain text language choice

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->encryptCookies(except: [
+            'locale',
+        ]);
+
         $middleware->web(append: [
             \App\Http\Middleware\SetAppLocale::class,
             \App\Http\Middleware\HandleInertiaRequests::class,
